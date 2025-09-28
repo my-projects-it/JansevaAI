d8553704328fb1b6975550d9b5a2b48864c6b4e1
barkha <107093216+my-projects-it@users.noreply.github.com>
2025-09-28 19:13:16 +0530

    Create firebase_config.py

---
+++ firebase_config.py
@@ -0,0 +1,25 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import streamlit as st
+import json
+
+# Google Sheets credentials from Streamlit Secrets
+scope = ["https://spreadsheets.google.com/feeds","https://www.googleapis.com/auth/drive"]
+creds_json = json.loads(st.secrets["gsheet_credentials"])
+creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_json, scope)
+client = gspread.authorize(creds)
+
+user_sheet = client.open("CitizenHelpUsers").sheet1
+feedback_sheet = client.open("CitizenHelpFeedback").sheet1
+
+def save_user(email, password):
+    user_sheet.append_row([email, password])
+
+def validate_user(email, password):
+    users = user_sheet.get_all_records()
+    for u in users:
+        if u["email"] == email and u["password"] == password:
+            return True
+    return False
+
+def save_feedback(user_emai
